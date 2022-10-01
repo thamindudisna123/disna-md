d85925fe6550391b927fa70a9aec68ed967a496e
thamindudisna123 <114249753+thamindudisna123@users.noreply.github.com>
2022-10-01 10:23:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,21 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94768084855'] //ur owner number
+global.ownername = "✮  🎀  𝒯𝒽𝒶𝓂𝒾𝓃𝒹𝓊 𝒟𝒾𝓈𝓃𝒶  🎀  ✮" //ur owner name
+global.ytname = "YT: SL Creative Tech" //ur yt chanel name
+global.location = "Sri lanka" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
+global.owner = ['94768084855']
+global.ownertag = '94768084855' //ur tag number
+global.botname = '✮  🎀  𝒯𝒽𝒶𝓂𝒾𝓃𝒹𝓊 𝒟𝒾𝓈𝓃𝒶  🎀  ✮Bot MD V4' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UCV9YD7Jt3M-wak61MPqWogw" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UCV9YD7Jt3M-wak61MPqWogw" //ur website to be displayed
+global.reactmoji = "🤞" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = " 🍰  🎀  𝒢𝑒𝓃𝒶𝓇𝑒𝓉𝑒𝒹 𝒷𝓎-𝒯𝒽𝒶𝓂𝒾𝓃𝒹𝓊 𝒟𝒾𝓈𝓃𝒶  🎀  🍰 " //ur sticker watermark packname
+global.author = " 🍰  🎀  𝒢𝑒𝓃𝒶𝓇𝑒𝓉𝑒𝒹 𝒷𝓎-𝒯𝒽𝒶𝓂𝒾𝓃𝒹𝓊 𝒟𝒾𝓈𝓃𝒶  🎀  🍰" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +77,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94768084855'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -105,16 +103,16 @@ global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
+    success: 'හරී ✓',
+    admin: 'Admin ට විතරයි ඔය command එක',
+    botAdmin: 'Bot Admin ට ඕක තියෙන්නෙ!',
+    owner: 'BOT Owner ට විතරයි!',
+    group: 'group වල විතරයි ඔය command එක දෙන්න පුලුවන් මේ group එකට add වෙන්න https://chat.whatsapp.com/LcLK8NZgeJ9GJfGITIKCvG',
+    private: 'මේක owner ට දාන මැසේජ් වලට විතරයි',
+    bot: 'මේක bot ට විතරයි',
+    wait: 'පොඩ්ඩක් ඉන්න...',
     linkm: 'Where is the link?',
-    error: 'Error!!',
+    error: 'පොඩි අවුලක් ආය දාන්න බලන්න!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
